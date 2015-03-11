2fe20a8d042f8c0f2fca46088957baf6df291c4f
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-11 11:55:19 -0400

    Added scripts

---
+++ FoodFrenzy/Assets/Scripts/MenuBuild.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+	public MenuInfo[] units;
+	
+	public static MenuInfo cur;
+	
+	void OnGUI() 
+	{
+		// Begin Gui
+		GUILayout.BeginArea(new Rect(Screen.width/2 - 100, -7, 200, 100));
+		GUILayout.BeginHorizontal("box");
+		
+		// Draw each unuits BuildInfo
+		foreach (MenuInfo bi in units) 
+		{
+			if (GUILayout.Button(new GUIContent(bi.price.ToString(), bi.previewImage)))
+			{
+				cur = bi;
+			}
+		}
+		// End Gui
+
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+	}
+
+}
---
+++ FoodFrenzy/Assets/Scripts/MenuInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInfo : MonoBehaviour {
+
+	public Texture previewImage;
+	public int price;
+
+}
