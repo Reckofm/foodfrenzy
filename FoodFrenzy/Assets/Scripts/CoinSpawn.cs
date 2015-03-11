01485b4f98a7a8d44a6c955d0362b546799301f1
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-11 14:35:59 -0400

    Added coin spawn

---
+++ FoodFrenzy/Assets/Scripts/CoinSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawn : MonoBehaviour {
+
+	public GameObject prefab;
+
+	void Start() {
+		// Spawn first Sun in 10 seconds, repeat every 10 seconds
+		InvokeRepeating("Spawn", 10, 10);
+	}
+	
+	void Spawn() {
+		Instantiate(prefab,
+		            transform.position,
+		            Quaternion.identity);
+	}
+}
