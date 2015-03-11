0590a64217cdae26f41208ba9c92af21c8ab4cc2
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-11 12:23:40 -0400

    Remaining scripts

---
+++ FoodFrenzy/Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDamage : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D co) 
+	{
+		if (co.tag == "enemy") 
+		{
+			// Deal Damage, destroy Bullet
+			co.GetComponent<Health>().doDamage(1);
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ FoodFrenzy/Assets/Scripts/BulletFire.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFire : MonoBehaviour {
+
+	public GameObject bullet;
+	
+	public float shootingInterval = 0.5f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating("Shoot", 0, shootingInterval);
+	}
+	
+	void Shoot() 
+	{
+		if (this.enemyInFront()) 
+		{
+			GetComponent<Animator> ().SetTrigger ("IsFiring");
+			
+			Instantiate (bullet, transform.position, Quaternion.identity);
+		}
+	}
+	
+	bool enemyInFront() 
+	{
+		// Raycast from the right of the game to the plant
+		Vector2 origin = new Vector2(9.5f, transform.position.y);
+		RaycastHit2D[] hits = Physics2D.RaycastAll(origin, -Vector2.right);
+		
+		// Find out if any Zombie was hit
+		foreach (RaycastHit2D hit in hits) 
+		{
+			if (hit.collider != null &&
+			    hit.collider.gameObject.tag == "enemy")
+				return true;
+		}
+		return false;
+	}
+
+}
