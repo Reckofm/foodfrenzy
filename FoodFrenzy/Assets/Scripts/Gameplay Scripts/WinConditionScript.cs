1362b53a91b59ed4a0ddb75379b837adf4f41417
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-18 11:28:58 -0400

    win condition

---
+++ FoodFrenzy/Assets/Scripts/Gameplay Scripts/WinConditionScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinConditionScript : MonoBehaviour {
+
+	public string sceneName;
+
+	// Update is called once per frame
+	void Update () {
+		if(GameObject.FindWithTag("Respawn") == null && GameObject.FindWithTag("Enemy") == null)
+		{
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
