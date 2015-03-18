209a7accfe6f30958a46c511a8ca0a89bbac189b
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-18 11:50:38 -0400

    Work on fade

--- FoodFrenzy/Assets/Scripts/Gameplay Scripts/WinConditionScript.cs
+++ FoodFrenzy/Assets/Scripts/Gameplay Scripts/WinConditionScript.cs
@@ -6,10 +6,19 @@ public class WinConditionScript : MonoBehaviour {
 	public string sceneName;
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if(GameObject.FindWithTag("Respawn") == null && GameObject.FindWithTag("Enemy") == null)
 		{
+			this.ChangeLevel();
 			Application.LoadLevel(sceneName);
 		}
+	}
+
+	IEnumerator ChangeLevel()
+	{
+		float fadeTime = GameObject.Find ("GameController").GetComponent<Fading>().BeginFade(1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel(sceneName);
 	}
 }
