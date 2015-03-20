c134999a1fbb75cfd70e0c150d72bcbe697c7feb
Sean McDonald <s.michael.mcdonald@gmail.com>
2015-03-19 22:54:08 -0400

    Spice rack base

---
+++ FoodFrenzy/Assets/Scripts/UI Scripts/SpiceRack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiceRack : MonoBehaviour {
+
+	public MenuInfo[] spices;
+
+	public static MenuInfo cur;
+	
+	void OnGUI() 
+	{
+		// Begin Gui
+		GUILayout.BeginArea(new Rect(Screen.width/2 - 150, Screen.height - 30, 300, 100));
+		GUILayout.BeginHorizontal("box");
+		
+		// Draw each unuits BuildInfo
+		foreach (MenuInfo bi in spices) 
+		{
+			if (GUILayout.Button(new GUIContent(bi.price.ToString(), bi.previewImage)))
+			{
+				cur = bi;
+			}
+		}
+		// End Gui
+		
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+	}
+}
